52fe8bb8c8661f71bf69b037f16c01281f18c6b1
Szymon Miareczka <szymiar@gmail.com>
2021-01-11 16:18:27 +0100

    Wstępny html

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import com.example.demo.model.Address;
 
 import com.example.demo.model.Animal;
+import com.example.demo.model.Person;
+import com.example.demo.model.Person2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,16 +20,18 @@ public class DemoApplication implements CommandLineRunner {
 	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 
 	@Override
 	public void run(String... args) throws Exception {
-		String sql ="select * from ANIMALS";
-		List<Animal> animals =jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Animal.class));
+		String sql ="select * from PERSONS";
 
-		System.out.println(animals.get(0).getSPECIES());
+		//List<Person2> persons =jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person2.class));
+
+		//System.out.println(persons.get(0).getSURNAME());
 
 
 	}
